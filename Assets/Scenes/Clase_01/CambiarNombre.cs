3c8d58de6f1714bc0544d1082bc820c168c45ad9
Monty <montoya.manzano.fernando@gmail.com>
2019-04-30 23:38:30 -0500

    Assets

---
+++ Assets/Scenes/Clase_01/CambiarNombre.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarNombre : MonoBehaviour
+{
+    public string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nombre = this.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.name = nombre;
+    }
+}
---
+++ Assets/Shooter/Scripts/MoveChar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChar : MonoBehaviour {
+    public Transform myBodyHead;
+    public Transform myBodyPlayer;
+    public float xSpeed = 1;
+    public float ySpeed = 1;
+    public float yaw, pitch;
+    public float walkSpeed=1;
+    public float runSpeed = 2;
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        if (myBodyPlayer == null)
+            myBodyPlayer = this.transform;
+        if (myBodyHead == null)
+            myBodyHead = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Mira();
+
+        Avanzar();
+
+    }
+
+    void Mira()
+    {
+        yaw += xSpeed * Input.GetAxis("Mouse X");
+        pitch -= xSpeed * Input.GetAxis("Mouse Y");
+        myBodyHead.transform.localEulerAngles = new Vector3(pitch, 0.0f, 0.0f);
+        myBodyPlayer.transform.localEulerAngles = new Vector3(0.0f, yaw, 0.0f);
+    }
+
+    void Avanzar()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) //correr
+                myBodyPlayer.localPosition += myBodyPlayer.forward * Time.deltaTime * runSpeed;
+            else //caminar
+                myBodyPlayer.localPosition += myBodyPlayer.forward * Time.deltaTime * walkSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myBodyPlayer.localPosition -= myBodyPlayer.forward * Time.deltaTime * walkSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myBodyPlayer.localPosition += myBodyPlayer.right * Time.deltaTime * walkSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myBodyPlayer.localPosition -= myBodyPlayer.right * Time.deltaTime * walkSpeed;
+        }
+    }
+
+
+}
